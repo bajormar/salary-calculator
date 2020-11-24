591937d2cfa528e94562a9a79763ab081e6727f5
Marius Bajorunas <marius@cornercasetech.com>
2020-11-24 15:02:45 +0200

    Allow to specify cell weights

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,6 @@
 import React, { useMemo, useState } from "react";
 import "./App.css";
 
-const devCells = [
-  "Autonomy",
-  "Delivery",
-  "Growth",
-  "Job knowledge",
-  "Soft skills",
-];
-
-const pmCells = [
-  "Capabilities",
-  "Quality of work",
-  "Communication",
-  "Autonomy",
-  "Job Knowledge / Growth / Initiative",
-];
-
-const teamLeadCells = [
-  "Hiring",
-  "TechLead",
-  "Team management",
-  "Soft skills",
-  "Mentoring",
-];
-
 export const times = (count: number) => Array.from(Array(count).keys());
 
 enum Role {
@@ -33,6 +9,30 @@ enum Role {
 }
 function App() {
   const [role, setRole] = useState(Role.Developer);
+
+  const [devSkills, setDevSkills] = useState([
+    { name: "Autonomy", weight: 0.2 },
+    { name: "Delivery", weight: 0.2 },
+    { name: "Growth", weight: 0.2 },
+    { name: "Job knowledge", weight: 0.2 },
+    { name: "Soft skills", weight: 0.2 },
+  ]);
+
+  const [pmSkills, setPmSkills] = useState([
+    { name: "Capabilities", weight: 0.2 },
+    { name: "Quality of work", weight: 0.2 },
+    { name: "Communication", weight: 0.2 },
+    { name: "Autonomy", weight: 0.2 },
+    { name: "Job Knowledge / Growth / Initiative", weight: 0.2 },
+  ]);
+
+  const [teamLeadSkills, setTeamLeadSkills] = useState([
+    { name: "Hiring", weight: 0.2 },
+    { name: "TechLead", weight: 0.2 },
+    { name: "Team management", weight: 0.2 },
+    { name: "Soft skills", weight: 0.2 },
+    { name: "Mentoring", weight: 0.2 },
+  ]);
 
   const [devLevels, setDevLevels] = useState([
     { name: "JUNIOR_1", low: 1000 },
@@ -62,47 +62,47 @@ function App() {
   const [selected, setSelected] = useState([0, 0, 0, 0, 0]);
   const [teamLeadSelected, setTeamLeadSelected] = useState([0, 0, 0, 0, 0]);
 
-  const cells = role === Role.Developer ? devCells : pmCells;
+  const skills = role === Role.Developer ? devSkills : pmSkills;
   const levels = role === Role.Developer ? devLevels : pmLevels;
+  const setSkills = role === Role.Developer ? setDevSkills : setPmSkills;
   const setLevels = role === Role.Developer ? setDevLevels : setPmLevels;
 
   const cellValues = useMemo(() => {
-    const junior1Values = [
-      Math.floor(levels[0].low / cells.length),
-      Math.floor(levels[0].low / cells.length),
-      Math.floor(levels[0].low / cells.length),
-      Math.floor(levels[0].low / cells.length),
-      Math.floor(levels[0].low / cells.length),
-    ];
+    const totalWeight = skills.reduce((acc, skill) => acc + skill.weight, 0);
+    const junior1Values = skills.map((skill) => {
+      return Math.floor((levels[0].low / totalWeight) * skill.weight);
+    });
 
     return [
       junior1Values,
       ...levels.slice(1).map((level) => {
         const range = level.high! - level.low;
-        const value = Math.floor(range / cells.length);
-        return times(cells.length + 1).map(() => value);
+        return skills.map((skill) => {
+          return Math.floor((range / totalWeight) * skill.weight);
+        });
       }),
     ];
-  }, [cells, levels]);
+  }, [skills, levels]);
 
   const teamLeadCellValues = useMemo(() => {
-    const lead11Values = [
-      Math.floor(teamLeadLevels[0].low / teamLeadCells.length),
-      Math.floor(teamLeadLevels[0].low / teamLeadCells.length),
-      Math.floor(teamLeadLevels[0].low / teamLeadCells.length),
-      Math.floor(teamLeadLevels[0].low / teamLeadCells.length),
-      Math.floor(teamLeadLevels[0].low / teamLeadCells.length),
-    ];
+    const totalWeight = teamLeadSkills.reduce(
+      (acc, skill) => acc + skill.weight,
+      0
+    );
+    const lead11Values = teamLeadSkills.map((skill) => {
+      return Math.floor((teamLeadLevels[0].low / totalWeight) * skill.weight);
+    });
 
     return [
       lead11Values,
       ...teamLeadLevels.slice(1).map((level) => {
         const range = level.high! - level.low;
-        const value = Math.floor(range / teamLeadCells.length);
-        return times(teamLeadCells.length + 1).map(() => value);
+        return teamLeadSkills.map((skill) => {
+          return Math.floor((range / totalWeight) * skill.weight);
+        });
       }),
     ];
-  }, [teamLeadLevels]);
+  }, [teamLeadLevels, teamLeadSkills]);
 
   const salary = useMemo(() => {
     return selected.reduce((acc, columnSelectedLevel, index) => {
@@ -138,34 +138,116 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <h2>Choose role</h2>
-        <button
-          type="button"
-          className={`RoleButton ${
-            role === Role.Developer ? "RoleButton-active" : ""
-          }`}
-          onClick={() => setRole(Role.Developer)}
-        >
-          Developer
-        </button>
-        <button
-          type="button"
-          className={`RoleButton ${
-            role === Role.ProjectManager ? "RoleButton-active" : ""
-          }`}
-          onClick={() => setRole(Role.ProjectManager)}
-        >
-          Project manager
-        </button>
+      <div className="Settings-Row">
+        <div>
+          <h2>Choose role</h2>
+          <button
+            type="button"
+            className={`RoleButton ${
+              role === Role.Developer ? "RoleButton-active" : ""
+            }`}
+            onClick={() => setRole(Role.Developer)}
+          >
+            Developer
+          </button>
+          <button
+            type="button"
+            className={`RoleButton ${
+              role === Role.ProjectManager ? "RoleButton-active" : ""
+            }`}
+            onClick={() => setRole(Role.ProjectManager)}
+          >
+            Project manager
+          </button>
+        </div>
+
+        {role === Role.Developer && (
+          <div>
+            <div>Is team lead?</div>
+            <input
+              type="checkbox"
+              checked={isTeamLead}
+              onChange={(e) => {
+                setIsTeamLead(!isTeamLead);
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <div className="Row">
+        <table>
+          <thead>
+            <tr>
+              <th>Skills</th>
+              <th>Weight</th>
+            </tr>
+          </thead>
+          <tbody>
+            {skills.map((skill) => (
+              <tr key={skill.name}>
+                <td>{skill.name}</td>
+                <td>
+                  <input
+                    type="text"
+                    defaultValue={skill.weight}
+                    onChange={(e) => {
+                      setSkills((prevState) => {
+                        const newState = [...prevState];
+                        const skillToEdit = newState.find(
+                          (item) => item.name === skill.name
+                        );
+                        skillToEdit!.weight = Number(e.target.value || 0) || 0;
+                        return newState;
+                      });
+                    }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {role === Role.Developer && isTeamLead && (
+          <table>
+            <thead>
+              <tr>
+                <th>Team lead skills</th>
+                <th>Weight</th>
+              </tr>
+            </thead>
+            <tbody>
+              {teamLeadSkills.map((skill) => (
+                <tr key={skill.name}>
+                  <td>{skill.name}</td>
+                  <td>
+                    <input
+                      type="text"
+                      defaultValue={skill.weight}
+                      onChange={(e) => {
+                        setTeamLeadSkills((prevState) => {
+                          const newState = [...prevState];
+                          const skillToEdit = newState.find(
+                            (item) => item.name === skill.name
+                          );
+                          skillToEdit!.weight =
+                            Number(e.target.value || 0) || 0;
+                          return newState;
+                        });
+                      }}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <div className="Row">
         <table>
           <thead>
             <tr>
               <th>Level</th>
-              {cells.map((cell) => (
-                <th key={cell}>{cell}</th>
+              {skills.map((skill) => (
+                <th key={skill.name}>{skill.name}</th>
               ))}
             </tr>
           </thead>
@@ -173,23 +255,23 @@ function App() {
             {levels.map((level, levelIndex) => (
               <tr key={level.name}>
                 <td>{level.name}</td>
-                {cells.map((cell, cellIndex) => (
+                {skills.map((skill, skillIndex) => (
                   <td
-                    key={cell}
+                    key={skill.name}
                     style={
-                      levelIndex <= selected[cellIndex]
+                      levelIndex <= selected[skillIndex]
                         ? { background: "lightblue" }
                         : {}
                     }
                     onClick={() =>
                       setSelected((prevState) => {
                         const newState = [...prevState];
-                        newState[cellIndex] = levelIndex;
+                        newState[skillIndex] = levelIndex;
                         return newState;
                       })
                     }
                   >
-                    {cellValues[levelIndex][cellIndex]}
+                    {cellValues[levelIndex][skillIndex]}
                   </td>
                 ))}
               </tr>
@@ -255,128 +337,110 @@ function App() {
         </table>
         <div>Salary: {getSalaryRanges(salary)} EUR</div>
       </div>
-      {role === Role.Developer && (
+      {role === Role.Developer && isTeamLead && (
         <>
           <div className="Row">
-            <div>
-              <div>Is team lead?</div>
-              <input
-                type="checkbox"
-                checked={isTeamLead}
-                onChange={(e) => {
-                  setIsTeamLead(!isTeamLead);
-                }}
-              />
-            </div>
-          </div>
-          {isTeamLead && (
-            <>
-              <div className="Row">
-                <table>
-                  <thead>
-                    <tr>
-                      <th></th>
-                      {teamLeadCells.map((cell) => (
-                        <th key={cell}>{cell}</th>
-                      ))}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {teamLeadLevels.map((level, levelIndex) => (
-                      <tr key={level.name}>
-                        <td>{level.name}</td>
-                        {teamLeadCells.map((cell, cellIndex) => (
-                          <td
-                            key={cell}
-                            style={
-                              levelIndex <= teamLeadSelected[cellIndex]
-                                ? { background: "lightblue" }
-                                : {}
-                            }
-                            onClick={() =>
-                              setTeamLeadSelected((prevState) => {
-                                const newState = [...prevState];
-                                newState[cellIndex] = levelIndex;
-                                return newState;
-                              })
-                            }
-                          >
-                            {teamLeadCellValues[levelIndex][cellIndex]}
-                          </td>
-                        ))}
-                      </tr>
+            <table>
+              <thead>
+                <tr>
+                  <th></th>
+                  {teamLeadSkills.map((skill) => (
+                    <th key={skill.name}>{skill.name}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {teamLeadLevels.map((level, levelIndex) => (
+                  <tr key={level.name}>
+                    <td>{level.name}</td>
+                    {teamLeadSkills.map((skill, skillIndex) => (
+                      <td
+                        key={skill.name}
+                        style={
+                          levelIndex <= teamLeadSelected[skillIndex]
+                            ? { background: "lightblue" }
+                            : {}
+                        }
+                        onClick={() =>
+                          setTeamLeadSelected((prevState) => {
+                            const newState = [...prevState];
+                            newState[skillIndex] = levelIndex;
+                            return newState;
+                          })
+                        }
+                      >
+                        {teamLeadCellValues[levelIndex][skillIndex]}
+                      </td>
                     ))}
-                  </tbody>
-                </table>
-                <table>
-                  <thead>
-                    <tr>
-                      <th>Level</th>
-                      <th>Low</th>
-                      <th>High</th>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <table>
+              <thead>
+                <tr>
+                  <th>Level</th>
+                  <th>Low</th>
+                  <th>High</th>
+                </tr>
+              </thead>
+              <tbody>
+                {teamLeadLevels.map((level) => {
+                  return (
+                    <tr key={level.name}>
+                      <td>
+                        {level.name === "LEAD_1"
+                          ? `${level.name} (Base)`
+                          : level.name}
+                      </td>
+                      <td>
+                        <input
+                          type="text"
+                          value={level.low}
+                          onChange={(e) => {
+                            setTeamLeadLevels((prevState) => {
+                              const newState = [...prevState];
+                              const levelToEdit = newState.find(
+                                (l) => l.name === level.name
+                              );
+                              levelToEdit!.low =
+                                Number(e.target.value || 0) || 0;
+                              return newState;
+                            });
+                          }}
+                        />
+                      </td>
+                      <td>
+                        {level.name !== "LEAD_1" && (
+                          <input
+                            type="text"
+                            value={level.high}
+                            onChange={(e) => {
+                              setTeamLeadLevels((prevState) => {
+                                const newState = [...prevState];
+                                const levelToEdit = newState.find(
+                                  (l) => l.name === level.name
+                                );
+                                levelToEdit!.high =
+                                  Number(e.target.value || 0) || 0;
+                                return newState;
+                              });
+                            }}
+                          />
+                        )}
+                      </td>
                     </tr>
-                  </thead>
-                  <tbody>
-                    {teamLeadLevels.map((level) => {
-                      return (
-                        <tr key={level.name}>
-                          <td>
-                            {level.name === "LEAD_1"
-                              ? `${level.name} (Base)`
-                              : level.name}
-                          </td>
-                          <td>
-                            <input
-                              type="text"
-                              value={level.low}
-                              onChange={(e) => {
-                                setTeamLeadLevels((prevState) => {
-                                  const newState = [...prevState];
-                                  const levelToEdit = newState.find(
-                                    (l) => l.name === level.name
-                                  );
-                                  levelToEdit!.low =
-                                    Number(e.target.value || 0) || 0;
-                                  return newState;
-                                });
-                              }}
-                            />
-                          </td>
-                          <td>
-                            {level.name !== "LEAD_1" && (
-                              <input
-                                type="text"
-                                value={level.high}
-                                onChange={(e) => {
-                                  setTeamLeadLevels((prevState) => {
-                                    const newState = [...prevState];
-                                    const levelToEdit = newState.find(
-                                      (l) => l.name === level.name
-                                    );
-                                    levelToEdit!.high =
-                                      Number(e.target.value || 0) || 0;
-                                    return newState;
-                                  });
-                                }}
-                              />
-                            )}
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-                <div>
-                  Team lead salary: {getSalaryRanges(teamLeadSalary)} EUR
-                </div>
-              </div>
-              <div className="Row">
-                <div />
-                <div />
-                <div>Total salary: {getSalaryRanges(totalSalary)} EUR</div>
-              </div>
-            </>
-          )}
+                  );
+                })}
+              </tbody>
+            </table>
+            <div>Team lead salary: {getSalaryRanges(teamLeadSalary)} EUR</div>
+          </div>
+          <div className="Row">
+            <div />
+            <div />
+            <div>Total salary: {getSalaryRanges(totalSalary)} EUR</div>
+          </div>
         </>
       )}
     </div>
